6990f00400f8d3a5cf018a4c3e68d52c36da5811
dev44hiko <128977461+dev44hiko@users.noreply.github.com>
2023-03-26 23:22:43 +0900

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,34 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * Hello world!
+ * Main App
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "main Start" );
+        
+        String customer = "BigCo";
+        List<Performance> performances = new ArrayList<>();
+        performances.add(new Performance("hamlet", 55));
+        performances.add(new Performance("as-like", 35));
+        performances.add(new Performance("othello", 40));
+        Invoice invoice = new Invoice(customer, performances);
+
+        Map<String, Play> plays = new HashMap<>();
+        plays.put("hamlet", new Play("Hamlet", "tragedy"));
+        plays.put("as-like", new Play("As You Like It", "comedy"));
+        plays.put("othello", new Play("Othello", "tragedy"));
+        
+        Accounting accounting = new Accounting();
+        String result = accounting.statement(invoice, plays);
+        System.out.println(result);
     }
 }
